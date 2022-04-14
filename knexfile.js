524f3be00fcd066299dc99c5c58ce2c28541101c
Nicole Valentini <ncvalentini@gmail.com>
2022-04-13 19:34:29 -0600

    Remove second export

--- knexfile.js
+++ knexfile.js
@@ -24,13 +24,13 @@ module.exports = {
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
-module.exports = {
-	development: {
-		client: 'pg',
-		connection: 'postgres://localhost/bird_data',
-		migrations: {
-			directory: './db/migrations',
-		},
-		useNullAsDefault: true,
-	},
-}
+// module.exports = {
+// 	development: {
+// 		client: 'pg',
+// 		connection: 'postgres://localhost/bird_data',
+// 		migrations: {
+// 			directory: './db/migrations',
+// 		},
+// 		useNullAsDefault: true,
+// 	},
+// }
