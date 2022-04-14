434ca947bb1ebb1a569c8b9e5d3692844c8f5c2f
Nicole Valentini <ncvalentini@gmail.com>
2022-04-13 18:42:11 -0600

    Remove exports duplicate

--- knexfile.js
+++ knexfile.js
@@ -24,13 +24,3 @@ module.exports = {
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
-module.exports = {
-	development: {
-		client: 'pg',
-		connection: 'postgres://localhost/bird_data',
-		migrations: {
-			directory: './db/migrations',
-		},
-		useNullAsDefault: true,
-	},
-}
